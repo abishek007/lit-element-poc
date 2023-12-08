c97a6b20f75496ff9c7497d451d2a3c7724cd8c8
Abishek D <abishek@Abisheks-MacBook-Pro.local>
2023-12-08 10:26:35 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
@@ -5,7 +6,76 @@ import { customElement } from 'lit/decorators.js';
 export default class TestElement extends LitElement {
   render() {
     return html`
-            <h1>Hello World!</p>
+    <select>
+      <option value="value1">Option 1</option>
+      <option value="value2">Option 2</option>
+      <option value="value3">Option 3</option>
+    </select>
         `;
   }
 }
+*/
+
+import { LitElement, html, css } from 'lit';
+
+class DropdownComponent extends LitElement {
+  static styles = css`
+    /* Define your CSS styles for the dropdown here */
+    /* Example styles for the dropdown */
+    :host {
+      display: inline-block;
+      position: relative;
+    }
+
+    .dropdown {
+      position: absolute;
+      display: none;
+      background-color: #fff;
+      border: 1px solid #ccc;
+      padding: 5px;
+      z-index: 1;
+    }
+
+    :host([opened]) .dropdown {
+      display: block;
+    }
+  `;
+
+  static properties = {
+    options: { type: Array },
+    selectedOption: { type: String },
+    opened: { type: Boolean, reflect: true }, // Reflect the opened state to an attribute
+  };
+
+  constructor() {
+    super();
+    this.options = ["value1", "value2", "value3"];
+    this.selectedOption = '';
+    this.opened = false;
+  }
+
+  render() {
+    return html`
+      <div @click="${this.toggleDropdown}">
+        Selected Option: ${this.selectedOption}
+      </div>
+      <div class="dropdown">
+        ${this.options.map(
+      option =>
+        html`<div @click="${() => this.selectOption(option)}">${option}</div>`
+    )}
+      </div>
+    `;
+  }
+
+  toggleDropdown() {
+    this.opened = !this.opened;
+  }
+
+  selectOption(option) {
+    this.selectedOption = option;
+    this.opened = false;
+  }
+}
+
+customElements.define('dropdown-component', DropdownComponent);
